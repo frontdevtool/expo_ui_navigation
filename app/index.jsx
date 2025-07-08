1bae90161ec788fc380cc03c827eb8e43c6d2e3e
frontdevtool <kazem84@hotmail.com>
2025-07-08 16:35:27 +0100

    react native svg

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 
 import "./global.css";
+import WelcomScreen from "@/screens/WelcomScreen";
 export default function index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>index</Text>
+     <WelcomScreen/>
     </View>
   );
 }
