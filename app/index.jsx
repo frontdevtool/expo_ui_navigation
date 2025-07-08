a62cf32cc13c3b61eefc08a54d070809b88896c5
frontdevtool <kazem84@hotmail.com>
2025-07-08 06:54:23 +0100

    nativeWind

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import  './global.css'
 
 const index = () => {
   return (
-    <View style={{flex:1, justifyContent :'center' , alignItems :'center'}}>
-      <Text >index page</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   )
 }
