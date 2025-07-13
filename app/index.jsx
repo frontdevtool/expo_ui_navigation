c7712f2fb944a4170f026c09ed46bd9b93e35b41
frontdevtool <kazem84@hotmail.com>
2025-07-13 11:40:10 +0100

    paper_ui

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,22 @@
 import { View, Text } from "react-native";
+import { Button } from 'react-native-paper';
 
 import "./global.css";
 import WelcomScreen from "@/screens/WelcomScreen";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function index() {
-  return (
-    <View className="flex-1 justify-center items-center">
-     <WelcomScreen/>
-    </View>
-  );
+
+
+return (
+<View className="">
+    <Text>index</Text>
+   
+    <Button icon="account-arrow-down" mode="contained" onPress={()=> console.log('Pressed')}>
+        Press me
+    </Button>
+
+
+
+</View>
+);
 }
